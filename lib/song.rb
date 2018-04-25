02ea2f5b20f896adebe52f3b89d77d1f34092ec3
Joseph Parker <jparker010190@gmail.com>
2018-04-25 01:28:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   
 def self.find_or_create_by_name(name)
     if self.find_by_name(name) == nil
-      self.find_by_name(name)
+      self.create_by_name_by_name(name)
     else
       self.create_by_name(name)
     end
